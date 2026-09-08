7e01ae692fbdd32ad65ee138748438acaf2f7d7d
Solomon <solomonyamal89@gmail.com>
2026-09-08 21:06:25 +0300

    feat(api): proxy contact api in dev mode to send real emails

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://solianwolves-website.vercel.app',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 })
