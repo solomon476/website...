7551669fada385bae68f1720c0d087fdf92d200e
Solomon <solomonyamal89@gmail.com>
2026-09-12 22:45:17 +0300

    revert: remove cropped photos and revert Solomon bio layout to original

--- src/pages/team/Solomon.jsx
+++ src/pages/team/Solomon.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PageNav from '../../components/PageNav.jsx';
 import '../../pages.css';
-import solomonImg1 from '../../assets/solomon-1-cropped.jpg';
-import solomonImg2 from '../../assets/solomon-2-cropped.jpg';
+import solomonImg from '../../assets/solomon.jpg';
 
 export default function Solomon() {
   return (
@@ -13,8 +12,7 @@ export default function Solomon() {
       </nav>
       <div className="bio-page">
         <div className="bio-image-col">
-          <img src={solomonImg1} alt="Solomon Nyangweso Omwenga - Image 1" className="bio-portrait" style={{ marginBottom: '24px' }} />
-          <img src={solomonImg2} alt="Solomon Nyangweso Omwenga - Image 2" className="bio-portrait" />
+          <img src={solomonImg} alt="Solomon Nyangweso Omwenga" className="bio-portrait" />
         </div>
         <div className="bio-content-col">
           <span className="page-tag" style={{ background: '#0067b8' }}>Chief Technology Officer</span>
